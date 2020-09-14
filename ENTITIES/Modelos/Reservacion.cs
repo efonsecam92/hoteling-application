8ea4a0c859e1f71afda7fad1049768b26bbd07e3
Esteban Fonseca <71184363+efonsecam92@users.noreply.github.com>
2020-09-14 14:48:19 -0600

    Add files via upload

---
+++ ENTITIES/Modelos/Reservacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.Modelos
+{
+    public class Reservacion
+    {
+        public int ID_Reservacion { get; set; }
+        public DateTime Fecha_Desde { get; set; }
+        public DateTime Fecha_Hasta { get; set; }
+        public string Nombre_Cliente { get; set; }
+        public int Cant_Adultos { get; set; }
+        public int Cant_Ninos { get; set; }
+        public int ID_Usuario { get; set; }
+        public int ID_Estado { get; set; }
+        public string Nombre_Estado { get; set; }
+        public int? ID_Cliente { get; set; }
+        public List<SGR_Reservaciones_Habitaciones> listaReservacionHabitacion { get; set; }
+    }
+}
